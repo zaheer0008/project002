670e153ea07334f2f33f0fbed3c2f1a0d732fc16
zaheer0008 <zaheer0008@gmail.com>
2020-11-08 19:10:28 +0500

    Assignment 2 completed. bug removed.

--- src/index.js
+++ src/index.js
@@ -3,25 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-function Room(){
-  const [temp, setTemp] =React.useState(22);
-  const state = React.useState(true);
-  const isLit = state[0];
-  const setLit = state[1];
-  const brightness = isLit ? 'lit' : 'dark';
-
-  return(
-    <div className={brightness}>
-      <p>The room is {isLit ? 'lit' : 'dark'}.</p>
-      <button onClick={()=> setLit(!isLit)}>Toggle</button>
-      <button onClick={()=>{setLit(true)}}>On</button>
-      <button onClick={()=>{setLit(false)}}>Off</button>
-      <p>Room Temperature is {temp} Celsius.</p>
-      <button onClick={()=>{setTemp(temp+1)}}>+</button>
-      <button onClick={()=>{setTemp(temp-1)}}>-</button>
-    </div>
-  );
-}
 
 function Reddit(){
     const [posts, setPosts] = React.useState([]);
